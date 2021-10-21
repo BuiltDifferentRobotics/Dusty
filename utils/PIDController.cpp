87d2c2d8773c493e233c8d0d94110ca2b18aaf6d
ankitatmidnight <82774113+ankitatmidnight@users.noreply.github.com>
2021-10-20 19:45:58 -0500

    Update PIDController.cpp

--- utils/PIDController.cpp
+++ utils/PIDController.cpp
@@ -1,34 +1,28 @@
 class PIDController {
 public:
 	PIDController(double KP, double KI, double KD, double tolerance);
-	//
+
 	double calculate(double sensor, double target);
 	void setKP(double KP);
 	void setKI(double KI);
 	void setKD(double KD);
-	void setTolerance(double Tolerance);
-	void setI_zone(double I_zone);
-	void setNormal_Min(double Normal_Min);
-	void setNormal_Max(double Normal_Max);
-	//
-	double getKP();
-	double getKI();
-	double getKD();
-	double getTolerance();
-	double getI_zone();
-	double getNormal_min();
-	double getNormal_max();
 private:
 	double KP_, KI_, KD_;
 	double tolerance_;
-	double I_zone_;
-	double Normal_Min_, Normal_Max_;
-	//long lastTime, currTime;
+	double I_zone;
+	double Normal_Min, Normal_Max;
+	double integral = 0;
+	double lastError = 0;
+	double deltaTime;
+	double initError = NULL;
+
+	double P_Comp;
+	double I_Comp;
+	double D_Comp;
+
+	double returnVal;
 };
 
-//Implementation
-
-//constructor
 PIDController::PIDController(double KP, double KI, double KD, double tolerance) {
 	KP_ = KP;
 	KI_ = KI;
@@ -36,48 +30,20 @@ PIDController::PIDController(double KP, double KI, double KD, double tolerance) 
 	tolerance_ = tolerance;
 }
 
-//setters
-void PIDController::setKP (double KP) {
-	KP_ = KP;
-}
-void PIDController::setKI(double KI) {
-	KI_ = KI;
-}
-void PIDController::setKD(double KD) {
-	KD_ = KD;
-}
-void PIDController::setTolerance(double Tolerance) {
-	tolerance_ = Tolerance;
-}
-void PIDController::setI_zone(double I_zone) {
-	I_zone_ = I_zone;
-}
-void PIDController::setNormal_Min(double Normal_Min) {
-	Normal_Min_ = Normal_Min;
-}
-void PIDController::setNormal_Min(double Normal_Max) {
-	Normal_Max_ = Normal_Max;
-}
+PIDController::calculate(double sensor, double target, double deltatime) {
+	double error = sensor - target;
+	//P
+	P_Comp = KP_ * error;
+	//I
+	integral += (error + lastError)* deltaTime/2;
+	I_Comp = KI_ * integral;
+	//D
+	D_Comp = KD_ * (error - lastError) / deltaTime;
 
-//getters
-double PIDController::getKP() {
-	return KP_;
-}
-double PIDController::getKI() {
-	return KI_;
-}
-double PIDController::getKD() {
-	return KD_;
-}
-double PIDController::getTolerance() {
-	return tolerance_;
-}
-double PIDController::getI_zone() {
-	return I_zone_;
-}
-double PIDController::getNormal_min() {
-	return Normal_Min_;
-}
-double PIDController::getNormal_max() {
-	return Normal_Max_;
+	lastError = error;
+	returnVal = P_Comp + I_Comp + D_Comp;
+	returnVal = (abs(returnVal) < Normal_Min) ? Normal_Min : returnVal;
+	returnVal = (abs(returnVal) > Normal_Max) ? Normal_Max : returnVal;
+	
+	return returnVal;
 }
